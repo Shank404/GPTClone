608db3298809b5971fc38026571b481d4b49c6db
Nico Isheim <nico.isheim@tutanota.com>
2023-08-30 20:40:29 +0200

    Read and display training data

--- src/train.py
+++ src/train.py
@@ -1,1 +1,24 @@
-data = wget(https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt)
+# Read training data
+with open("./data/input.txt", "r", encoding="utf-8") as textIOWrapper:
+    text = textIOWrapper.read()
+
+# Print length of training data
+# print("Length of training data: ", len(text))
+
+# Print first 1000 characters of training data
+# print(text[:1000])
+
+# Get a set of all unique characters in the training data
+training_set = set(text)
+
+# Get a list of all the data in the training set
+training_list = list(training_set)
+
+# Sort the list of training data
+training_list.sort()
+
+# Print the length of the training list
+# print("Number of unique characters in the training data: ", len(training_list))
+
+# Print all the unique characters in the training list
+# print("Characters: ", training_list)
